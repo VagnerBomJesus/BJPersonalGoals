18497316e3fb56bd9d9dd0b682d0764d8dbed22d
Vagner Bom Jesus <48354097+VagnerBomJesus@users.noreply.github.com>
2024-09-11 15:16:26 +0000

    criar a primeira rota Post insert to goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
+import z from 'zod'
+import { createGoal } from '../functions/create-gal'
 
 const app = fastify()
+
+app.post('/goals', async request => {
+  // Implement createGoal logic here
+
+  const createGoalScheme = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalScheme.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
