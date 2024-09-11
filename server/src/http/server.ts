4673e454d15f25d8f6437ad7d16ebb5915fb3907
Vagner Bom Jesus <48354097+VagnerBomJesus@users.noreply.github.com>
2024-09-11 16:12:59 +0000

    create function get week pending goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,6 +7,7 @@ type ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { createGoal } from '../functions/create-gal'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -15,7 +16,14 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.get('/pending-goals', async () => {
 
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return {
+    pendingGoals,
+  }
+})
 
 app.post('/goals',{
   schema:{
